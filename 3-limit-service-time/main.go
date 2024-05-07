7024a8dbbd326b07e92cb27cde5b510f3d037c94
benyamin <benyaminsa@gmail.com>
2024-05-07 12:26:00 +0300

    small refactor

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -17,8 +17,8 @@ import (
 
 // Free processing time allowed per user, in seconds
 const (
-	FreeTimeAllowed int64         = 10
-	Quantom         time.Duration = time.Second
+	FreeQuantaAllowed int64         = 10
+	Quantom           time.Duration = time.Second
 )
 
 // User defines the UserModel. Use this to check whether a User is a
@@ -29,27 +29,28 @@ type User struct {
 	TimeUsed  int64 // in seconds
 }
 
-// HandleRequest runs the processes requested by users. Returns false
-// if process had to be killed
-func HandleRequest(process func(), u *User) bool {
-	if u.IsPremium {
-		// no time limit for paying premium users
-		process()
-		return true
-	}
-	// else, free trial model
+// handles premium user's requests
+func handlePremiumUser(process func()) bool {
+	// no time limit for premium users
+	process()
+	return true
+}
+
+// handles un-paying users trail mode requests
+func handleFreeUser(process func(), u *User) bool {
 	// if the user exhaused the free trial, return false
-	if atomic.LoadInt64(&u.TimeUsed) >= FreeTimeAllowed {
+	if atomic.LoadInt64(&u.TimeUsed) >= FreeQuantaAllowed {
 		return false
 	}
 	ticker := time.Tick(Quantom)
 	processDone := make(chan bool)
-	// run process in a go-routine and report once it's done, notice it terminate before full execution
+	// run process in a go-routine and report once it's done
+	// notice the process can be terminated before full execution
 	go func() {
 		process()
 		processDone <- true
 	}()
-	// update TimeUsed every quanta, ff the user exhausts the free trial, return false
+	// update TimeUsed every quantom, if the user exhausts the free trial, return false
 	// if the process is complete before that, return true
 	for {
 		select {
@@ -57,13 +58,24 @@ func HandleRequest(process func(), u *User) bool {
 		case <-ticker:
 			// atomic u.TimeUsed++
 			timeUsed := atomic.AddInt64(&u.TimeUsed, 1)
-			if timeUsed >= FreeTimeAllowed {
+			if timeUsed >= FreeQuantaAllowed {
 				return false
 			}
 		case <-processDone:
 			return true
 		}
 	}
+
+}
+
+// HandleRequest runs the processes requested by users. Returns false
+// if process had to be killed
+func HandleRequest(process func(), u *User) bool {
+	if u.IsPremium {
+		return handlePremiumUser(process)
+	}
+	// else, free trial model
+	return handleFreeUser(process, u)
 }
 
 func main() {
